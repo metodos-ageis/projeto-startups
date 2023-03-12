94722216b0ff5750cd9efe0622d1b2e32712f44c
Mateus Lins <mateuslins02@gmail.com>
2023-03-12 15:54:28 -0300

    new changes to register company page

--- src/pages/RegisterCompany/RegisterCompany.tsx
+++ src/pages/RegisterCompany/RegisterCompany.tsx
@@ -19,26 +19,30 @@ function RegisterCompany() {
   return (
     <div className="flex items-center justify-center h-screen p-8">
       <div className="max-w-5xl max-h-[80vh] h-full w-full bg-slate-100 shadow-lg p-8">
-        <Text variant="h3" className="pb-4">
+        <IconButton className="pl-8 pb-4">
+          <Icon className="pr-2" fontSize="large">arrow_back</Icon>
+          <Text>Voltar</Text>
+        </IconButton>
+        <Text variant="h2" className="pb-16 text-center">
           {t("Title")}
         </Text>
         <div className="grid grid-rows-1 grid-cols-2 gap-4">
           <div className="w-80 flex flex-col gap-4">
+            <Text variant="h3">{t("TitleCol1")}</Text>
             <Input name="startup-name" type="text" label={t("Form.Name")} onChange={setName} value={name} />
             <Selector items={sectors} label="Setor" />
+            <Input name="initial-capital" type="text" label={t("Form.Monetary")} onChange={setName} value={name} />
             <TextArea label="Descrição" />
           </div>
-          <div className="w-80">
-            <IconButton>
-              <Icon>
-                arrow_back
-              </Icon>
-            </IconButton>
-
-            <Button variant="contained">
-              Salvar
-            </Button>
-
+          <div className="w-80 flex flex-col gap-4">
+            <Text variant="h3">{t("TitleCol2")}</Text>
+            <Input name="representative-name" type="text" label={t("Form.RepName")} onChange={setName} value={name} />
+            <Input name="representative-email" type="text" label={t("Form.RepEmail")} onChange={setName} value={name} />
+            <div className="place-self-end">
+              <Button variant="contained">
+                Salvar
+              </Button>
+            </div>
           </div>
         </div>
       </div>
--- src/translations/pt-BR/register-startup.ts
+++ src/translations/pt-BR/register-startup.ts
@@ -1,6 +1,11 @@
 export default {
   Title: "Cadastrar incubada",
+  TitleCol1: "Incubada",
+  TitleCol2: "Representante",
   Form: {
     Name: "Nome da incubada",
+    Monetary: "Capital inicial",
+    RepName: "Nome do representante",
+    RepEmail: "Email do representante",
   }
 };
