841ebcd03aa9d64b9009572bdda779767ec36329
Mateus Lins <mateuslins02@gmail.com>
2023-03-13 22:45:35 -0300

    new fields to register company page

--- src/pages/RegisterCompany/RegisterCompany.tsx
+++ src/pages/RegisterCompany/RegisterCompany.tsx
@@ -14,30 +14,55 @@ const sectors: SelectorItem<string>[] = [
 function RegisterCompany() {
   const { t } = useTranslation("registerStartup");
 
-  const [name, setName] = useFormState("");
+  const [title, setTitle] = useFormState("");
+  const [description, setDescription] = useFormState("");
+  const [sector, setSector] = useFormState("");
+  const [goal, setGoal] = useFormState("");
+  const [cnpj, setCnpj] = useFormState("");
+  const [project_time, setProjectTime] = useFormState("");
+  const [challenges, setChallenges] = useFormState("");
+  const [team_size, setTeamSize] = useFormState("");
+  const [project_started, setProjectStarted] = useFormState("");
+  const [website, setWebsite] = useFormState("");
+  const [ip, setIp] = useFormState("");
+  const [resourcers, setResources] = useFormState("");
+  const [resourcers_needed, setResourcesNeeded] = useFormState("");
+  const [mvp, setMvp] = useFormState("");
+  const [incoming_model, setIncomingModel] = useFormState("");
+  const [initial_capital, setInitialCapital] = useFormState("");
 
   return (
     <div className="flex items-center justify-center h-screen p-8">
-      <div className="max-w-5xl max-h-[80vh] h-full w-full bg-slate-100 shadow-lg p-8">
+      <div className="max-w-5xl max-h-[96vh] h-full w-full bg-slate-100 shadow-lg p-8">
         <IconButton className="pl-8 pb-4">
           <Icon className="pr-2" fontSize="large">arrow_back</Icon>
           <Text>Voltar</Text>
         </IconButton>
-        <Text variant="h2" className="pb-16 text-center">
+        <Text variant="h2" className="pb-8 text-center">
           {t("Title")}
         </Text>
         <div className="grid grid-rows-1 grid-cols-2 gap-4">
           <div className="w-80 flex flex-col gap-4">
-            <Text variant="h3">{t("TitleCol1")}</Text>
-            <Input name="startup-name" type="text" label={t("Form.Name")} onChange={setName} value={name} />
-            <Selector items={sectors} label="Setor" />
-            <Input name="initial-capital" type="text" label={t("Form.Monetary")} onChange={setName} value={name} />
-            <TextArea label="Descrição" />
+            {/* <Text variant="h3">{t("TitleCol1")}</Text> */}
+            <Input name="startup-name" type="text" label={t("Form.Name")} onChange={setTitle} value={title} />
+            <Selector items={sectors} label={t("Form.Sector")} />
+            <TextArea label={t("Form.Description")} />
+            <Input name="initial-capital" type="text" label={t("Form.Monetary")} onChange={setInitialCapital} value={initial_capital} />
+            <Input name="incoming-model" type="text" label={t("Form.IncomingModel")} onChange={setIncomingModel} value={incoming_model} />
+            <Input name="resources" type="text" label={t("Form.Resources")} onChange={setResources} value={resourcers} />
+            <Input name="resources-needed" type="text" label={t("Form.ResourcesNeeded")} onChange={setResourcesNeeded} value={resourcers_needed} />
+            <Input name="intelectual-property" type="text" label={t("Form.IP")} onChange={setIp} value={ip} />
           </div>
           <div className="w-80 flex flex-col gap-4">
-            <Text variant="h3">{t("TitleCol2")}</Text>
-            <Input name="representative-name" type="text" label={t("Form.RepName")} onChange={setName} value={name} />
-            <Input name="representative-email" type="text" label={t("Form.RepEmail")} onChange={setName} value={name} />
+            {/* <Text variant="h3">{t("TitleCol2")}</Text> */}
+            <Input name="cnpj" type="text" label={t("Form.Cnpj")} onChange={setCnpj} value={cnpj} />
+            <Input name="goal" type="text" label={t("Form.Goal")} onChange={setGoal} value={goal} />
+            <Input name="mvp" type="text" label={t("Form.MVP")} onChange={setMvp} value={mvp} />
+            <Input name="project-time" type="text" label={t("Form.ProjectTime")} onChange={setProjectTime} value={project_time} />
+            <Input name="challenges" type="text" label={t("Form.Challenges")} onChange={setChallenges} value={challenges} />
+            <Input name="team-size" type="text" label={t("Form.TeamSize")} onChange={setTeamSize} value={team_size} />
+            <Input name="project-start" type="text" label={t("Form.ProjectStarted")} onChange={setProjectStarted} value={project_started} />
+            <Input name="website" type="text" label={t("Form.Website")} onChange={setWebsite} value={website} />
             <div className="place-self-end">
               <Button variant="contained">
                 Salvar
--- src/translations/pt-BR/register-startup.ts
+++ src/translations/pt-BR/register-startup.ts
@@ -5,7 +5,19 @@ export default {
   Form: {
     Name: "Nome da incubada",
     Monetary: "Capital inicial",
-    RepName: "Nome do representante",
-    RepEmail: "Email do representante",
+    Sector: "Setor",
+    Description: "Descrição",
+    Cnpj: "CNPJ",
+    Goal: "Objetivo",
+    ProjectTime: "Tempo do projeto (dias)",
+    Challenges: "Desafios",
+    TeamSize: "Tamanho do time",
+    ProjectStarted: "Data de início do projeto",
+    Website: "Website",
+    IP: "Propriedade intelectual",
+    Resources: "Recursos",
+    ResourcesNeeded: "Recursos necessários",
+    MVP: "MVP",
+    IncomingModel: "Modelo de receita",
   }
 };
