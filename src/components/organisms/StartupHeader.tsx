3c1a46596c835049f563fa61734608daeea13426
Vinícius Souza <vinicius.rsouza07@gmail.com>
2023-04-10 20:51:59 -0300

    fix: header button

--- src/components/organisms/StartupHeader.tsx
+++ src/components/organisms/StartupHeader.tsx
@@ -1,14 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
 
 import { StartupBadge, IconButton } from "../molecules";
 
 import { Icon, Surface } from "@/components/atoms";
+import { useEventCallback } from "@/hooks";
 
 interface Props {
   className?: string;
 }
 
 function Header({ className }: Props) {
+  const navigate = useNavigate();
+  const onBack = useEventCallback(() => navigate(-1));
   return (
     <Surface
       className={twMerge(
@@ -17,7 +21,7 @@ function Header({ className }: Props) {
       )}
     >
       <div className="grid grid-cols-[repeat(3,_auto)] justify-between items-center w-full">
-        <IconButton className="ml-8">
+        <IconButton className="ml-8" onClick={onBack}>
           <Icon fontSize="2extra-large">arrow_back</Icon>
         </IconButton>
         <img src="assets/logo.jpeg" className="w-44 my-8" />
